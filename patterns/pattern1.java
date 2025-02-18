7377da73e8eda77c3c05405c9dc5e591418fb58d
Love Sahu <lovevsahu2003@gmail.com>
2025-02-19 03:10:06 +0530

    Update pattern1.java

--- patterns/pattern1.java
+++ patterns/pattern1.java
@@ -1,11 +1,19 @@
 class pattern1{
   public static void main(String args[]){
-int n=15;
-    for(int i=1;i<=n;i++){
-for(int j=1;j<=i;j++){
-System.out.print("* ");
-}
+       int n=5;
+       for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+        System.out.print("* ");
+       }
       System.out.println();
     }
   }
 }
+//output 
+/*
+*
+* *
+* * *
+* * * *
+* * * * *
+  */
