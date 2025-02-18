d10ece9e408aa9c46751830f9c8125c4f345f60e
Love Sahu <lovevsahu2003@gmail.com>
2025-02-19 03:17:50 +0530

    Update and rename pattern2 to pattern2.java
    hollo right angle trangle

---
+++ patterns/pattern2.java
@@ -0,0 +1,26 @@
+class pattern2{
+public static void main(string args[]){
+int n=5;
+ for(int i=1;i<=n;i++){
+  for(int j=1;j<=i;j++){
+   if(i==j||i==1||i==n){
+   System.out.print("* ");
+   }
+   System.out.println();
+   }
+  }
+ }
+}
+
+//op 
+/*
+
+  
+*
+*  *
+*     *
+*         *
+*  *  *  *  *
+
+
+  */
