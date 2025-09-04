4ac890c10d89b9846ac3719cc1e052188fbec0f1
Love Sahu <lovevsahu2003@gmail.com>
2025-09-04 13:41:13 -0700

    Add files via upload

---
+++ Collection/Collectionclass.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class MyCollection
+{
+ public static void reverse(list <E>){
+ArrayList<E> li=new ArrayList<>();
+for(int i=list.size();i>=0;i--){
+	li.set(i,ele);
+   }
+ }
+}
+
+class Collectionclass 
+{
+	public static void main(String[] args) 
+	{
+		ArrayList<Integer> list=new ArrayList<>();
+		list.add(120);
+		list.add(140);
+		list.add(150);
+		list.add(180);
+		System.out.println(list);		
+		//Collections.shuffle(list);
+		MyCollections.reverse(list);
+	
+		System.out.println(list);
+	}
+     
+}
